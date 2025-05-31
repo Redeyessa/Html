9b82698a2653eb05d2e90a6b8ddb780cde3c9757
sanjay.R <nr.sanjay848@gmail.com>
2025-05-31 21:45:37 +0530

    all

---
+++ project/main.js
@@ -0,0 +1,23 @@
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+    import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyA9m77QBvEVC_NWYHTn0G1q7dLdql82WTs",
+      authDomain: "freeott-780a3.firebaseapp.com",
+      projectId: "freeott-780a3",
+      storageBucket: "freeott-780a3.firebasestorage.app",
+      messagingSenderId: "73605407408",
+      appId: "1:73605407408:web:5dd1cf0ba336cd7f519175",
+      measurementId: "G-RQ15FHRTFG"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    // 🔒 Redirect if not logged in
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        window.location.href = "loginpage.html"; // redirect to login
+      }
+    });
