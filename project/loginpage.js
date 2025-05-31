98c55c441cdacf1d6fe505933a4067494623548e
sanjay.R <nr.sanjay848@gmail.com>
2025-05-31 12:58:47 +0530

    aa

---
+++ project/loginpage.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA9m77QBvEVC_NWYHTn0G1q7dLdql82WTs",
+    authDomain: "freeott-780a3.firebaseapp.com",
+    projectId: "freeott-780a3",
+    storageBucket: "freeott-780a3.firebasestorage.app",
+    messagingSenderId: "73605407408",
+    appId: "1:73605407408:web:5dd1cf0ba336cd7f519175",
+    measurementId: "G-RQ15FHRTFG"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
