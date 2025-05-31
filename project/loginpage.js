62e4c593ed43b4ffe006be98fc7ef618ab0f3456
sanjay.R <nr.sanjay848@gmail.com>
2025-05-31 14:02:52 +0530

    ll

--- project/loginpage.js
+++ project/loginpage.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "www.gstatic.com/firebasejs/11.8.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,13 +21,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
+const auth = getAuth();
 
 const submit = document.getElementById('submit');
 
 submit.addEventListener("click", function (event) {
   event.preventDefault()
-  const auth = getAuth();
   const email = document.getElementById('Email').value;
   const password = document.getElementById('password').value;
   createUserWithEmailAndPassword(auth, email, password)
